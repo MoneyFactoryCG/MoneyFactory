f1b208c844eb2c419c075cf88d60e7dd37bbb777
MoneyFactoryCG <moneyfactory.creativgroup@gmail.com>
2019-10-28 16:03:04 +0200

    change test

--- functions/test.js
+++ functions/test.js
@@ -9,6 +9,7 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 app.use(bodyParser);
 app.post("/send", (res, req) => {
   bot.sendMessage(-361781942, req.body.phone);
+  console.log("ok");
 });
 
 module.exports.handler = serverless(app);
