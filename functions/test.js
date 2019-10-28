acd27003af72049315870c803743e67eba1b5817
MoneyFactoryCG <moneyfactory.creativgroup@gmail.com>
2019-10-28 17:10:09 +0200

    fix test

--- functions/test.js
+++ functions/test.js
@@ -8,6 +8,11 @@ const bot = new Telegraf("905553028:AAH3MRBKNmS8y3qpxFvOCkTeNExFDHL07xw");
 const router = express.Router();
 
 app.use(bodyParser.json());
+router.get("/", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("<h1>Hello from MoneyFactory!</h1>");
+  res.end();
+});
 router.post("/", (req, res) => {
   bot.telegram.sendMessage(-361781942, req.body.phone);
   res.json({ send: "ok" });
