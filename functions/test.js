d2fb84126af944926b22907c22d63c93b00fbdbf
MoneyFactoryCG <moneyfactory.creativgroup@gmail.com>
2019-10-28 16:16:22 +0200

    fix test

--- functions/test.js
+++ functions/test.js
@@ -5,11 +5,15 @@ const bodyParser = require("body-parser");
 const Telegraf = require("telegraf");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+const router = express.Router();
 
-app.use(bodyParser);
-app.post("/send", (res, req) => {
+app.use(bodyParser.json());
+router.post("/", (res, req) => {
   bot.sendMessage(-361781942, req.body.phone);
   res.status(200).json({ send: "ok" });
 });
 
+app.use("/.netlify/functions/test", router);
+
 module.exports.handler = serverless(app);
+module.exports = app;
