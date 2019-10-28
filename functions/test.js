b5498ac36b827dadb192223e8472ae29c2eb35a5
MoneyFactoryCG <moneyfactory.creativgroup@gmail.com>
2019-10-28 16:07:43 +0200

    fix post send

--- functions/test.js
+++ functions/test.js
@@ -9,7 +9,7 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 app.use(bodyParser);
 app.post("/send", (res, req) => {
   bot.sendMessage(-361781942, req.body.phone);
-  console.log("ok");
+  req.status(200).json({ send: "ok" });
 });
 
 module.exports.handler = serverless(app);
